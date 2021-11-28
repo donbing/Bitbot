ce31732da3ae4f7b3c3ddc665a683d59392c2b5b
donbing <ccbing@gmail.com>
2021-11-28 23:33:38 +0000

    uncomppress 3 day view

--- src/currency_chart.py
+++ src/currency_chart.py
@@ -8,7 +8,8 @@ import matplotlib.dates as mdates
 import random
 
 def fetch_OHLCV_chart_data(candleFreq, chartDuration, config):
-    startdate = datetime.utcnow() - chartDuration
+    today = datetime.combine(datetime.today(), datetime.min.time())
+    startdate = today - chartDuration
     exchange = config["currency"]["exchange"]
     instrument = config["currency"]["instrument"]
     print('fetching data from ' + exchange)
@@ -80,9 +81,9 @@ class chart_data:
     def __init__(self, config):   
         layouts = [
             ('1d', timedelta(days=60), 0.01, mdates.DayLocator(interval=7), mdates.DateFormatter('%d'), mdates.MonthLocator(), mdates.DateFormatter('%B')),
-            ('1h', timedelta(hours=60), 0.005, mdates.HourLocator(interval=4), mdates.DateFormatter(''), mdates.DayLocator(), mdates.DateFormatter('%D')),
+            ('1h', timedelta(hours=40), 0.005, mdates.HourLocator(interval=4), mdates.DateFormatter(''), mdates.DayLocator(), mdates.DateFormatter('%D')),
             ('1h', timedelta(hours=24), 0.01, mdates.HourLocator(interval=1), mdates.DateFormatter(''), mdates.HourLocator(interval=4), mdates.DateFormatter('%I %p')),
-            ('5m', timedelta(minutes=5*60), 0.0005, mdates.MinuteLocator(interval=10), mdates.DateFormatter(''), mdates.HourLocator(interval=1), mdates.DateFormatter('%I %p'))
+            ('5m', timedelta(minutes=5*60), 0.0005, mdates.MinuteLocator(interval=30), mdates.DateFormatter(''), mdates.HourLocator(interval=2), mdates.DateFormatter('%I%p'))
         ]
         
         self.layout = layouts[random.randrange(len(layouts))]
