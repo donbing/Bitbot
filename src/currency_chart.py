82e34b8016c0adeaaae2063b80a99725e36c8876
donbing <ccbing@gmail.com>
2022-01-04 15:11:18 +0000

    fix timezone issue

--- src/currency_chart.py
+++ src/currency_chart.py
@@ -1,11 +1,12 @@
 import ccxt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import mpl_finance
 import matplotlib.dates as mdates
 import random
+import tzlocal
 
 def fetch_OHLCV_chart_data(candleFreq, chartDuration, config):
     startdate = datetime.utcnow() - chartDuration
@@ -24,8 +25,14 @@ def fetch_OHLCV_chart_data(candleFreq, chartDuration, config):
     #print(" supported markets: " + " ".join(exchange.markets.keys()))
 
     candleData = exchange.fetchOHLCV(instrument, candleFreq, limit=1000, params={'startTime':startdate})
-    # clean up dates in data
-    return list(map(lambda x: replace_at_index(x, 0, mdates.date2num(datetime.utcfromtimestamp(x[0]/1000))), candleData))
+    
+    return list(map(lambda x: clean_element(x), candleData))
+
+def clean_element(element):
+    datetime_field = element[0]/1000
+    dateime_utc = datetime.fromtimestamp(datetime_field)
+    dateime_num = mdates.date2num(dateime_utc)
+    return replace_at_index(element, 0, dateime_num)
 
 def replace_at_index(tup, ix, val):
    lst = list(tup)
@@ -47,6 +54,7 @@ def get_plot():
     plt.rcParams['text.antialiased'] = False
     plt.rcParams['lines.antialiased'] = False
     plt.rcParams['patch.antialiased'] = False
+    plt.rcParams['timezone'] = tzlocal.get_localzone_name()
     return (fig, ax)
 
 def human_format(num, pos):
@@ -72,17 +80,17 @@ def configure_axes(ax, minor_format, minor_locator, major_format, major_locator)
     ax.xaxis.set_major_formatter(major_locator)
     # human readable short-foprmat y-axis currency amount
     ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(human_format))
-    ax.xaxis_date()
+    ax.xaxis_date(tz=datetime.now(timezone.utc).astimezone().tzinfo)
     # this will hide the axis/labels
     ax.autoscale_view(tight=False)
 
 class chart_data:
     def __init__(self, config):   
         layouts = [
-            ('1d', timedelta(days=60), 0.01, mdates.DayLocator(interval=7), mdates.DateFormatter('%d'), mdates.MonthLocator(), mdates.DateFormatter('%B')),
-            ('1h', timedelta(hours=40), 0.005, mdates.HourLocator(interval=4), mdates.DateFormatter(''), mdates.DayLocator(), mdates.DateFormatter('%D')),
-            ('1h', timedelta(hours=24), 0.01, mdates.HourLocator(interval=1), mdates.DateFormatter(''), mdates.HourLocator(interval=4), mdates.DateFormatter('%I %p')),
-            ('5m', timedelta(minutes=5*60), 0.0005, mdates.MinuteLocator(interval=30), mdates.DateFormatter(''), mdates.HourLocator(interval=2), mdates.DateFormatter('%I%p'))
+            #('1d', timedelta(days=60), 0.01, mdates.DayLocator(interval=7), mdates.DateFormatter('%d'), mdates.MonthLocator(), mdates.DateFormatter('%B')),
+            #('1h', timedelta(hours=40), 0.005, mdates.HourLocator(interval=4), mdates.DateFormatter(''), mdates.DayLocator(), mdates.DateFormatter('%D')),
+            #('1h', timedelta(hours=24), 0.01, mdates.HourLocator(interval=1), mdates.DateFormatter(''), mdates.HourLocator(interval=4), mdates.DateFormatter('%I %p')),
+            ('5m', timedelta(minutes=5*60), 0.0005, mdates.MinuteLocator(interval=30), mdates.DateFormatter(''), mdates.HourLocator(interval=1), mdates.DateFormatter('%I%p'))
         ]
         
         self.layout = layouts[random.randrange(len(layouts))]
