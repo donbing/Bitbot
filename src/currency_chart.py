da8ce92abefeb5db9b4420bb219b805accf024e4
donbing <ccbing@gmail.com>
2022-01-15 18:47:19 +0000

    remove day names from month chart

--- src/currency_chart.py
+++ src/currency_chart.py
@@ -106,7 +106,7 @@ class chart_data:
     def __init__(self, config, display):   
         fig, ax = get_plot(display)
         layouts = [
-            ('1d', 60, 0.01, mdates.DayLocator(interval=7), mdates.DateFormatter('%d'), mdates.MonthLocator(), mdates.DateFormatter('%B')),
+            ('1d', 60, 0.01, mdates.DayLocator(interval=7), mdates.DateFormatter(''), mdates.MonthLocator(), mdates.DateFormatter('%B')),
             ('1h', 40, 0.005, mdates.HourLocator(interval=4), mdates.DateFormatter(''), mdates.DayLocator(), mdates.DateFormatter('%a %d %b')),
             ('1h', 24, 0.01, mdates.HourLocator(interval=1), mdates.DateFormatter(''), mdates.HourLocator(interval=4), mdates.DateFormatter('%I %p')),
             ('5m', 60, 0.0005, mdates.MinuteLocator(interval=30), mdates.DateFormatter(''), mdates.HourLocator(interval=1), mdates.DateFormatter('%I%p'))
