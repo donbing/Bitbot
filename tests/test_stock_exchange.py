d24f3ee0fe07bae796d486c51945176ada0ee22e
donbing <ccbing@gmail.com>
2022-01-23 17:42:37 +0000

    sigh, 1h fails on weekends!

--- tests/test_stock_exchange.py
+++ tests/test_stock_exchange.py
@@ -6,16 +6,17 @@ from src import stock_exchanges
 from src.configuration import bitbot_config
 
 # '1wk' <- failes for reasons as yet unknown
-test_params = ["1mo", '1h',  'random']
+test_params = ["1mo", '1wk',  'random']
 
 
 class test_stock_exchange(unittest.TestCase):
     def test_fetcing_history(self):
         for candle_width in test_params:
             with self.subTest(msg=candle_width):
+                stock = "TSLA"
                 mock_config = {
                     "currency": {
-                        "stock_symbol": "TSLA"
+                        "stock_symbol": stock
                     },
                     "display": {
                         "candle_width": candle_width,
@@ -24,4 +25,4 @@ class test_stock_exchange(unittest.TestCase):
                 }
                 excange = stock_exchanges.Exchange(bitbot_config.BitBotConfig(mock_config))
                 data = excange.fetch_history()
-                self.assertTrue(len(data.candle_data) > 0, msg=f'got {len(data.candle_data)} candles')
+                self.assertTrue(len(data.candle_data) > 0, msg=f'got {len(data.candle_data)} candles for {stock}')
