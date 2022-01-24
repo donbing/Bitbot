749a7aa422350983ef01daf0c33b1826b925a6a0
donbing <ccbing@gmail.com>
2022-01-24 00:00:01 +0000

    lint run.py

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-import pathlib, logging, logging.config, sched, time, os
-from src.configuration.bitbot_files import use_config_dir  
-from src.configuration.bitbot_config import load_config_ini 
-from src.configuration.bitbot_logging import initialise_logger 
-from src.configuration.config_observer import watch_config_dir 
+import pathlib
+import logging
+import logging.config
+import sched
+import time
+import os
+from src.configuration.bitbot_files import use_config_dir
+from src.configuration.bitbot_config import load_config_ini
+from src.configuration.bitbot_logging import initialise_logger
+from src.configuration.config_observer import watch_config_dir
 from src.log_decorator import info_log
 from src.bitbot import BitBot
 
@@ -12,21 +17,22 @@ config_files = use_config_dir(pathlib.Path(__file__).parent.resolve())
 initialise_logger(config_files.logging_ini)
 # load app config
 config = load_config_ini(config_files.config_ini)
-
 # create bitbot chart updater
 app = BitBot(config, config_files)
 
+
 @info_log
-def refresh_chart(sc): 
+def refresh_chart(sc):
     app.run()
     # show image in vscode for debug
     if config.shoud_show_image_in_vscode():
-        os.system("code last_display.png")  
+        os.system("code last_display.png")
     # dont reschedule if testing
     if not config.is_test_run():
         refresh_minutes = config.refresh_rate_minutes()
         logging.info("Next refresh in: " + str(refresh_minutes) + " mins")
         sc.enter(refresh_minutes * 60, 1, refresh_chart, (sc,))
+
 
 @info_log
 def cancel_schedule(sc):
@@ -37,6 +43,7 @@ def cancel_schedule(sc):
             # This is OK because the event may have been just canceled
             pass
 
+
 @info_log
 def config_changed(sc):
     # reload the app config
@@ -46,11 +53,14 @@ def config_changed(sc):
     # new schedule
     refresh_chart(sc)
 
+
 # scheduler for regular chart updates
 scheduler = sched.scheduler(time.time, time.sleep)
 
 # refresh chart on config file change
-watch_config_dir(config_files.config_folder, on_changed = lambda: config_changed(scheduler))
+watch_config_dir(
+    config_files.config_folder,
+    on_changed=lambda: config_changed(scheduler))
 
 # update chart immediately and begin schedule
 refresh_chart(scheduler)
