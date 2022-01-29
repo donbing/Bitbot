b10a632f7289e078fd3fad9dc52751cbf6658f3c
donbing <ccbing@gmail.com>
2022-01-29 14:39:57 +0000

    same picture files to base path to avoid config trigger

--- src/configuration/bitbot_files.py
+++ src/configuration/bitbot_files.py
@@ -9,6 +9,7 @@ def use_config_dir(base_config_path):
 
 class BitBotFiles():
     def __init__(self, base_path):
+        self.base_path = base_path
         self.log_file_path = pjoin(base_path, 'debug.log')
 
         self.config_folder = pjoin(base_path, 'config/')
