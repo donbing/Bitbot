a2e3503736b05c3abaa4b758c3d8485a81f7378d
donbing <ccbing@gmail.com>
2022-01-31 19:28:07 +0000

    log harder

--- src/configuration/config_observer.py
+++ src/configuration/config_observer.py
@@ -1,4 +1,3 @@
-
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileModifiedEvent
 from src.configuration.log_decorator import info_log
@@ -29,16 +28,23 @@ class ConfigChangeHandler(FileSystemEventHandler):
         if isinstance(event, FileModifiedEvent):
             file_path = event.src_path
 
-            file_hash = get_hash(file_path)
             cached_file_hash = self.watched_files.get(file_path)
+            file_hash = get_hash(file_path)
 
-            new_change = file_path not in self.watched_files
-            file_changed = file_hash != cached_file_hash
+            self.check_if_file_changed(file_path, cached_file_hash, file_hash)
 
-            if new_change or file_changed:
-                self.config_changed(file_path, file_hash)
+    @info_log
+    def check_if_file_changed(self, file_path, cached_file_hash, file_hash):
+        new_change = file_path not in self.watched_files
+        file_changed = file_hash != cached_file_hash
+
+        if new_change or file_changed:
+            self.config_changed(file_path, file_hash)
 
     @info_log
     def config_changed(self, file_path, file_hash):
         self.watched_files[file_path] = file_hash
         self.on_changed()
+
+    def __repr__(self):
+        return '<Config watcher>'
