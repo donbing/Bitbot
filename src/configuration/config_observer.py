acd07e252fa1be2bd3be5f3856bd9c84f610b4e3
donbing <ccbing@gmail.com>
2022-01-31 20:07:02 +0000

    extract method and renaming

--- src/configuration/config_observer.py
+++ src/configuration/config_observer.py
@@ -18,7 +18,7 @@ def watch_config_dir(config_dir_path, on_changed):
 class ConfigChangeHandler(FileSystemEventHandler):
     def __init__(self, on_changed):
         self.on_changed = on_changed
-        self.watched_files = {}
+        self.file_hashes = {}
 
     def on_modified(self, event):
         if isinstance(event, FileModifiedEvent):
@@ -27,7 +27,7 @@ class ConfigChangeHandler(FileSystemEventHandler):
     def compare_content_hash(self, src_path):
         file_content = self.get_file_content(src_path)
         if len(file_content) > 0:
-            cached_hash = self.watched_files.get(src_path)
+            cached_hash = self.file_hashes.get(src_path)
             current_hash = compute_hash(file_content)
             self.check_file_changed(src_path, cached_hash, current_hash)
 
@@ -36,16 +36,16 @@ class ConfigChangeHandler(FileSystemEventHandler):
             return file.read()
 
     @info_log
-    def check_file_changed(self, file_path, cached_file_hash, file_hash):
-        new_change = file_path not in self.watched_files
-        file_changed = file_hash != cached_file_hash
+    def check_file_changed(self, file_path, cached_hash, file_hash):
+        new_change = file_path not in self.file_hashes
+        file_changed = file_hash != cached_hash
 
         if new_change or file_changed:
             self.config_changed(file_path, file_hash)
 
     @info_log
     def config_changed(self, file_path, file_hash):
-        self.watched_files[file_path] = file_hash
+        self.file_hashes[file_path] = file_hash
         self.on_changed()
 
     def __repr__(self):
