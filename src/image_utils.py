ae732c59230f2d150e164ce2c1b8cc88e170f43c
donbing <ccbing@gmail.com>
2022-02-02 20:41:04 +0000

    apparently, the robot was on the wrong side

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,3 +1,6 @@
+
+padding = 10
+
 def draw_centered_text(draw, message, font, display_size, pos='centre'):
     # 🌌 calculate space needed for message
     message_size = draw.textsize(
@@ -7,8 +10,10 @@ def draw_centered_text(draw, message, font, display_size, pos='centre'):
     # 📏 where to position the message
     if pos == 'centre':
         message_x, message_y = center_aligned(display_size,  message_size)
-    else:
+    elif pos == 'topright':
         message_x, message_y = top_right_aligned(display_size,  message_size)
+    elif pos == 'topleft':
+        message_x, message_y = top_left_aligned(display_size,  message_size)
 
     # 🖊️ draw the message at position
     draw.multiline_text(
@@ -19,7 +24,6 @@ def draw_centered_text(draw, message, font, display_size, pos='centre'):
         align="left")
 
     # 📏 position  for surrounding box
-    padding = 10
     x0, y0 = (message_x - padding, message_y - padding)
     x1 = message_x + message_size[0] + padding
     y1 = message_y + message_size[1] + padding
@@ -28,8 +32,11 @@ def draw_centered_text(draw, message, font, display_size, pos='centre'):
 
 
 def top_right_aligned(display_size, message_size):
-    padding = 10
     return (display_size[0] - message_size[0] - padding - 1, padding)
+
+
+def top_left_aligned(display_size, message_size):
+    return (0 + padding + 1, 0 + padding + 1)
 
 
 def center_aligned(display_size, message_size):
