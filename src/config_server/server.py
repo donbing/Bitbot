3e48dc185e05ce34136ce3d36552e1f109352af8
donbing <ccbing@gmail.com>
2022-02-06 23:50:12 +0000

    quick n dirty modal dialog for file edits

--- src/config_server/server.py
+++ src/config_server/server.py
@@ -1,26 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
+
 
 app = Flask(__name__)
 
-def find_config_directory_path(): 
-    default_config_dir = "../../config"
-    env_config_dir=os.getenv("BITBOT__CONFIG_DIR") 
+
+
+def find_config_directory_path():
+    default_config_dir = "/home/chris/bitbot/config/"
+    env_config_dir = os.getenv("BITBOT__CONFIG_DIR")
     return env_config_dir or default_config_dir
 
 
+# 🗂️ get all files in config directory
 config_dir = find_config_directory_path()
-print(config_dir)
-# list files in config dir
-config_file_paths = [file for file in os.listdir(config_dir) if os.path.isfile(os.path.join(config_dir, file))]
-print(config_file_paths)
+config_file_paths = [
+    file
+    for file in os.listdir(config_dir)
+    if os.path.isfile(os.path.join(config_dir, file))]
 
-style_files = list(filter(lambda file: file.endswith("mplstyle"), config_file_paths))
-ini_files = list(filter(lambda file: file.endswith("ini"), config_file_paths))
+
+def config_files_ending_with(ending):
+    return list(filter(lambda file: file.endswith(ending), os.listdir(config_dir)))
+
+
+# 🗂️ list style and config files in config dir
+style_files = config_files_ending_with("mplstyle")
+ini_files = config_files_ending_with("ini")
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', style_files=style_files, ini_files=ini_files)
-    
+    return render_template(
+        'index.html',
+        style_files=style_files,
+        ini_files=ini_files
+    )
+
+
+@app.route('/file/<file>', methods=['POST', 'GET'])
+def file(file):
+    if request.method == 'POST':
+        fileContent = request.form['file']
+        return redirect(request.url)
+    else:
+        file_path = os.path.join(config_dir, config_files_ending_with(file)[0])
+        with open(file_path) as f:
+            return render_template('file.html', file=f.read())
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- src/config_server/static/config_editor.js
+++ src/config_server/static/config_editor.js
@@ -1,3 +1,27 @@
-function editFile(evt){
-    alert(evt);
+// 🔗 open element href in a modal dialog
+function editFile(element)
+{
+    let dialog = element.nextElementSibling;
+    var request = new XMLHttpRequest();
+    request.open("GET", element.href, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4)
+        {
+            dialog.innerHTML = request.responseText;
+            dialog.showModal();
+        };
+    };
+    return false;
 }
+
+// ❌ close dialog on click outside
+document.querySelectorAll('dialog').forEach(item => 
+    item.addEventListener('click', event => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        if (event.clientY < rect.top || event.clientY > rect.bottom ||
+            event.clientX < rect.left || event.clientX > rect.right) {
+                event.currentTarget.close();
+        }
+    })
+);
