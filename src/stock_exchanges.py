4c73523949a10e50925607b99ffe1613bf67377b
donbing <ccbing@gmail.com>
2022-02-08 01:03:51 +0000

    more restoration

--- src/stock_exchanges.py
+++ src/stock_exchanges.py
@@ -1,106 +1,63 @@
-import yfinance
-import collections
-import random
-from datetime import datetime, timedelta
-import matplotlib.dates as mdates
+from PIL import Image
+import io
+from src import crypto_exchanges, stock_exchanges
+from src.market_chart import MarketChart
 from src.configuration.log_decorator import info_log
-import math
+from src.chart_overlay import ChartOverlay
+from src.kinky import picker as display_picker
+from src.network_utils import wait_for_internet_connection
 
 
-class Exchange():
-    CandleConfig = collections.namedtuple('CandleConfig', 'width duration')
-    candle_configs = [
-        CandleConfig('1mo', timedelta(weeks=4*24)),
-        CandleConfig('1h', timedelta(hours=40)),
-        CandleConfig('1wk', timedelta(weeks=60)),
-        CandleConfig('3mo', timedelta(weeks=12*24))
-    ]
-
-    def fetch_history(self, instrument, candle_width):
-        ticker = yfinance.Ticker(instrument)
-        candle_config = self.select_candle_config(candle_width)
-
-        end_date = datetime.utcnow()
-        start_date = end_date - candle_config.duration
-
-        history = self.get_stock_history(
-            ticker,
-            candle_config.width,
-            start_date,
-            end_date)
-
-        return CandleData(instrument, candle_width, history, ticker)
+class Cartographer():
+    def __init__(self, config, display, files):
+        self.market = MarketChart(config, display, files)
 
     @info_log
-    def get_stock_history(self, ticker, candle_width, start_date, end_date):
-        return ticker.history(
-            interval=candle_width,
-            start=start_date.strftime("%Y-%m-%d"),
-            end=end_date.strftime("%Y-%m-%d"))
+    def draw_to(self, chart_data, file_stream):
+        self.market.create_plot(chart_data).write_to_stream(file_stream)
 
-    def select_candle_config(self, candle_width):
-        if(candle_width == "random"):
-            return self.get_random_candle_config()
+    def __repr__(self):
+        return '<Cartographer>'
+
+
+class BitBot():
+    def __init__(self, config, files):
+        self.config = config
+        self.files = files
+        self.display = display_picker(config)
+        self.plot = Cartographer(self.config, self.display, self.files)
+
+    # 🏛️ stock or crypto exchange
+    def market_exchange(self):
+        if self.config.stock_symbol():
+            return stock_exchanges.Exchange(self.config)
         else:
-            candle_config = self.get_candle_config_matching(candle_width)
-            return candle_config
+            return crypto_exchanges.Exchange()
 
-    def get_candle_config_matching(self, configred_candle_width):
-        candle_config, = (
-                conf for conf in self.candle_configs
-                if conf.width == configred_candle_width
-            )
-        return candle_config
+    @info_log
+    def display_chart(self):
+        # 📡 await internet connection
+        wait_for_internet_connection(self.display.draw_connection_error)
+        # 📈 fetch chart data
+        chart_data = self.market_exchange().fetch_history(
+            exchange_name=self.config.exchange_name(),
+            instrument=self.config.instrument_name(),
+            candle_width=self.config.candle_width()
+        )
+        # 🖊️ draw the chart on the display
+        with io.BytesIO() as file_stream:
+            # 🖊️ draw chart plot to image
+            self.plot.draw_to(chart_data, file_stream)
+            chart_image = Image.open(file_stream)
+            # 🖊️ draw overlay on image
+            overlay = ChartOverlay(self.config, self.display, chart_data)
+            overlay.draw_on(chart_image)
+            # 📺 display the image
+            self.display.show(chart_image)
 
-    def get_random_candle_config(self):
-        randomised_index = random.randrange(len(self.candle_configs))
-        new_var = self.candle_configs[randomised_index]
-        return new_var
-
-    def __repr__(self):
-        return '<yfinance stock Exchange>'
-
-
-def make_matplotfriendly_date(element):
-    datetime_field = element[0]
-    return replace_at_index(element, 0, datetime_field)
-
-
-def replace_at_index(tup, ix, val):
-    lst = list(tup)
-    lst[ix] = val
-    return tuple(lst)
-
-
-class CandleData():
-    def __init__(self, instrument, candle_width, candle_data, ticker):
-        self.instrument = f'{instrument}/{ticker.info["currency"]}'
-        self.candle_width = candle_width
-        candle_data.reset_index(level=0, inplace=True)
-        self.candle_data = self.clean_candle_data(candle_data)
-
-    def clean_candle_data(self, candle_data):
-        return list(map(make_matplotfriendly_date, candle_data.to_numpy()))
-
-    def percentage_change(self):
-        current_price = self.last_close()
-        starting_price = self.start_price()
-        return ((current_price - starting_price) / current_price) * 100
-
-    def last_close(self):
-        all_closes = self.select_index_if_number(self.candle_data, 4)
-
-        return float(all_closes[-1])
-
-    def start_price(self):
-        all_closes = self.select_index_if_number(self.candle_data, 4)
-        return float(all_closes[0])
-
-    def select_index_if_number(self, list, index):
-        return [
-            item[index]
-            for item in list
-            if not math.isnan(item[index])]
+    @info_log
+    def display_photo(self):
+        self.display.show(Image.open(self.config.photo_image_file()))
 
     def __repr__(self):
-        return f'<{self.instrument} {self.candle_width} candle data>'
+        return f'<BitBot inky: {str(self.config.use_inky())}>'
