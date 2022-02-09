43277a827cb4e2f2823d0547241d4627c6fae5a3
donbing <ccbing@gmail.com>
2022-02-09 20:39:55 +0000

    ui layout for friendly config

--- src/configuration/log_decorator.py
+++ src/configuration/log_decorator.py
@@ -6,11 +6,11 @@ def info_log(func):
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
         signature = ", ".join(args_repr + kwargs_repr)
-        # 🪵 log method call to info
+        # 🌳 log method call to info
         logging.info(f"{func.__name__}: {signature}")
         # 🔨 do the real work
         result = func(*args, **kwargs)
-        # 🪵 log result to debug
+        # 🐞 log result to debug
         logging.debug(result)
         return result
     return wrapper
