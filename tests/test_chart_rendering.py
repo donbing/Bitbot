ed55e49fe3fc0821ade84b1b03b4c1a06f8f8717
donbing <ccbing@gmail.com>
2022-03-05 18:29:49 +0000

    test with disk pls

--- tests/test_chart_rendering.py
+++ tests/test_chart_rendering.py
@@ -12,7 +12,7 @@ files = use_config_dir(os.path.join(curdir, "../"))
 
 def load_config():
     config = load_config_ini(files)
-    config.set('display', 'output', 'inky')
+    config.set('display', 'output', 'disk')
     return config
 
 
