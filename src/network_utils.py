f82d7ec755a7ff1efb496c4f880589cd3d39d831
donbing <ccbing@gmail.com>
2022-03-05 18:32:34 +0000

    extract another network util

--- src/network_utils.py
+++ src/network_utils.py
@@ -3,6 +3,20 @@ import socket
 import time
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
 def network_connected(hostname="google.com"):
     # 📡 test if internet is available
     try:
