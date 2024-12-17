b337f2d79851b59e55a7946fa09137476e4f0298
Chris Bingham <Christopher.Bingham@Marken.com>
2024-12-17 00:59:51 +0000

    shufflee mpl styles into folder and update docs a bit

--- src/configuration/bitbot_files.py
+++ src/configuration/bitbot_files.py
@@ -23,13 +23,20 @@ class BitBotFiles():
         self.config_ini = self.existing_file_path('config.ini')
         self.logging_ini = self.existing_file_path('logging.ini')
 
-        self.base_style = self.existing_file_path('base.mplstyle')
-        self.default_style = self.existing_file_path('default.mplstyle')
-        self.volume_style = self.existing_file_path('volume.mplstyle')
-        self.small_screen_style = self.existing_file_path('small.mplstyle')
-        self.expanded_style = self.existing_file_path('default.expanded.mplstyle')
-        self.small_expanded_style = self.existing_file_path('small.expanded.mplstyle')
+        self.base_style = self.load_mpl_style('base.mplstyle')
+        self.default_style = self.load_mpl_style('default.mplstyle')
+        self.volume_style = self.load_mpl_style('volume.mplstyle')
+        self.small_screen_style = self.load_mpl_style('small.mplstyle')
+        self.expanded_style = self.load_mpl_style('default.expanded.mplstyle')
+        self.small_expanded_style = self.load_mpl_style('small.expanded.mplstyle')
 
+    def load_mpl_style(self, file_name):
+        file_path = pjoin(self.config_folder, 'mpl_styles', file_name)
+        if not exists(file_path):
+            self.file_not_found(file_path)
+        self.all_files[file_name] = file_path
+        return file_path
+            
     def existing_file_path(self, file_name):
         file_path = pjoin(self.config_folder, file_name)
         if not exists(file_path):
