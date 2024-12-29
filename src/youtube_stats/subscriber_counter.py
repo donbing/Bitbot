df84a66f4ebb51b57193dd399dfec37401b5d159
Chris Bingham <Christopher.Bingham@Marken.com>
2024-12-29 09:52:38 +0000

    config and dead key

--- src/youtube_stats/subscriber_counter.py
+++ src/youtube_stats/subscriber_counter.py
@@ -4,26 +4,23 @@ from ..configuration.network_utils import wait_for_internet_connection
 import json
 import requests
 
-page1 = '''
-Hi, 
-    I will count your youtube subscribers
-'''
-channel_id="UCAotflAHrgfuhK9Rw-C_-Ug"
-your_key="AIzaSyCFJ6-vHN9KgOE3a6mjdqBcG-pYlwcRGj4"
-
-url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={your_key}"
 transparent = (255, 0, 0, 0)
 
 class YouTubeSubscriberCount:
     def __init__(self, size, font, config):
+        self.config = config
         self.display_size = size
         self.font = font
         self.centre = tuple(dim / 2 for dim in self.display_size.size)
 
     def play(self):
+        channel_id=self.config.youtube_channelid()
+        your_key=self.config.youtube_key()
+        url = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={your_key}"
         http_response = requests.get(url)
         response_json = json.loads(http_response.text)
-        text_to_draw = "Subscribers" + response_json['items'][0]['statistics']['subscriberCount']
+        subscriber_count = response_json['items'][0]['statistics']['subscriberCount']
+        text_to_draw = f"{subscriber_count} Subscribers"
         img = Image.new("RGBA", self.display_size.size, transparent)
         draw = ImageDraw.Draw(img)
         centered_text(draw, text_to_draw, self.font, self.display_size, 'centre')
