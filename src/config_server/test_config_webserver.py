15123f4bf9803878daa1361239dc8295f1d12683
Chris Bingham <Christopher.Bingham@Marken.com>
2025-01-08 19:19:46 +0000

    fix cycling instruments

--- src/config_server/test_config_webserver.py
+++ src/config_server/test_config_webserver.py
@@ -11,30 +11,7 @@ class TestRequests(unittest.TestCase):
         self.server = None
         self.port = 21344
         self.host = ''
-        self.start_server()
-
-    def start_server(self):
-        self.server = HTTPServer((self.host, self.port), InkyHandler)
-        self.server_thread = threading.Thread(target=self.server.serve_forever)
-        self.server_thread.daemon = True
-        self.server_thread.start()
-        time.sleep(0.25)
-
-    def stop_server(self):
-        if self.server is None:
-            return
-        self.server.shutdown()
-        self.server_thread.join()
-
-    def tearDown(self):
-        self.stop_server()
-
-    def test_get(self):
-        req = request.Request(f"http://localhost:{self.port}/")
-        req.add_header('accept', 'application/json')
-        response = request.urlopen(req).read()
-        print(response)
-
+        # flask
 
 if(__name__ == '__main__'):
     unittest.main()
