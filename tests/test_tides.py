88744154bdc1e97f110de4282d7475c5ac689b14
Chris Bingham <Christopher.Bingham@Marken.com>
2025-02-09 14:31:49 +0000

    simplify test

--- tests/test_tides.py
+++ tests/test_tides.py
@@ -8,7 +8,5 @@ class TestTidalGraph(unittest.TestCase):
     def test_data_fetch_and_render(self):
         
         with io.BytesIO() as img_buf:
-            tidal_graph.render_tide_chart("0184", img_buf)
-            
-            image = Image.open(img_buf)
+            image = tidal_graph.render_tide_chart("0184", img_buf)
             image.save(f'tests/images/TidalGraph.png')
